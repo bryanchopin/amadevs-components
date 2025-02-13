fa5e7905a2683846a25c17f308b79053f9870f1d
bryanchopin <bryan.oliveros17@tectijuana.edu.mx>
2025-02-12 16:58:39 -0800

    add pixel canvas component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,54 +1,3 @@
-// import BtnHoverDelete from "@/app/components/BtnHoverDelete";
-// import BtnChangeDelete from "@/app/components/BtnChangeDelete";
-
-// export default function Home() {
-//   return (
-//     <div className="p-4">
-//       {/* Texto con gradiente */}
-//       <h1 className="text-6xl font-bold bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 bg-clip-text text-transparent text-center mt-12">
-//         Amadevs Components
-//       </h1>
-//       {/* tailwind text gradient too */}
-//       <h2 className="text-2xl font-bold bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 bg-clip-text text-transparent text-center mb-12">
-//         with <span className="bg-gradient-to-r from-blue-400 via-blue-500 to-blue-700 bg-clip-text text-transparent text-center">TailwindCSS</span>
-//       </h2>
-
-//       {/* Contenedor del grid */}
-//       <div className="grid grid-cols-6 grid-rows-6 gap-2">
-//         <div className="col-span-2 row-span-2 border border-gray-300 relative h-[150px] w-full flex items-center justify-center">
-//           <BtnHoverDelete />
-//         </div>
-//         <div className="col-span-2 row-span-2 border border-gray-300 relative h-[150px] w-full flex items-center justify-center">
-//           <BtnChangeDelete />
-//         </div>
-//         <div className="col-span-2 row-span-2 border border-gray-300 relative h-[150px] w-full flex items-center justify-center">
-//           {/* Aquí puedes agregar otro componente */}
-//         </div>
-//         <div className="col-span-2 row-span-2 border border-gray-300 relative h-[150px] w-full flex items-center justify-center">
-//           {/* Aquí puedes agregar otro componente */}
-//         </div>
-//         <div className="col-span-2 row-span-2 border border-gray-300 relative h-[150px] w-full flex items-center justify-center">
-//           {/* Aquí puedes agregar otro componente */}
-//         </div>
-//         <div className="col-span-2 row-span-2 border border-gray-300 relative h-[150px] w-full flex items-center justify-center">
-//           {/* Aquí puedes agregar otro componente */}
-//         </div>
-//         <div className="col-span-2 row-span-2 border border-gray-300 relative h-[150px] w-full flex items-center justify-center">
-//           {/* Aquí puedes agregar otro componente */}
-//         </div>
-//         <div className="col-span-2 row-span-2 border border-gray-300 relative h-[150px] w-full flex items-center justify-center">
-//           {/* Aquí puedes agregar otro componente */}
-//         </div>
-//         <div className="col-span-2 row-span-2 border border-gray-300 relative h-[150px] w-full flex items-center justify-center">
-//           {/* Aquí puedes agregar otro componente */}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-
-
 import BtnHoverDelete from "@/app/components/BtnHoverDelete";
 import BtnChangeDelete from "@/app/components/BtnChangeDelete";
 
@@ -72,8 +21,9 @@ export default function Home() {
         <div className="col-span-1 md:col-span-2 row-span-2 border border-gray-300 relative h-[150px] flex items-center justify-center">
           <BtnChangeDelete />
         </div>
-        <div className="col-span-1 md:col-span-2 row-span-2 border border-gray-300 relative h-[150px] flex items-center justify-center">
-          {/* Aquí puedes agregar otro componente */}
+        <div className="col-span-1 md:col-span-2 row-span-2 border border-gray-300 relative h-[150px] flex items-center justify-center p-4">
+          <div className="w-full h-full flex items-center justify-center border border-blue-500 rounded-lg">
+          </div>
         </div>
         <div className="col-span-1 md:col-span-2 row-span-2 border border-gray-300 relative h-[150px] flex items-center justify-center">
           {/* Aquí puedes agregar otro componente */}
